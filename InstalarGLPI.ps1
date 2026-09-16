5e75131195e1fae19291a6ca0a09ab7b41d20310
v1BrN <bruno-santana1992@hotmail.com>
2026-09-16 17:39:42 -0300

    Update InstalarGLPI.ps1 for version 0.6

--- InstalarGLPI.ps1
+++ InstalarGLPI.ps1
@@ -1,4 +1,9 @@
-Write-Host "ZELUSNET - Teste GLPI Agent"$UrlMSI = "https://github.com/v1BrN/zelusnet/releases/download/v0.5/GLPI-Agent-1.19-x64.msi"
+# =========================================
+# ZELUSNET - Instalar GLPI Agent
+# Versão 0.6
+# =========================================
+
+$UrlMSI = "https://github.com/v1BrN/zelusnet/releases/download/v0.5/GLPI-Agent-1.19-x64.msi"
 
 $HashEsperado = "F3F933A54BC325FFE0D6063E177874E05138DD887FE690ADEF337640E8D6335C"
 
@@ -6,35 +11,54 @@ $Pasta = "C:\ProgramData\ZELUSNET"
 
 $MSI = "$Pasta\GLPI-Agent-1.19-x64.msi"
 
+# Cria pasta
+
 if (!(Test-Path $Pasta))
 {
-    New-Item -ItemType Directory -Path $Pasta -Force | Out-Null
+    New-Item `
+        -ItemType Directory `
+        -Path $Pasta `
+        -Force | Out-Null
 }
 
+Write-Host ""
 Write-Host "Baixando MSI..."
 
 Invoke-WebRequest `
     -Uri $UrlMSI `
     -OutFile $MSI
 
+if (!(Test-Path $MSI))
+{
+    Write-Host "Falha no download"
+
+    exit 1
+}
+
+Write-Host "Download concluído"
+
+Write-Host ""
 Write-Host "Validando SHA256..."
 
 $HashAtual =
 (
     Get-FileHash `
-    $MSI `
-    -Algorithm SHA256
+        $MSI `
+        -Algorithm SHA256
 ).Hash
 
 if ($HashAtual -ne $HashEsperado)
 {
-    Write-Host "Hash inválido"
+    Write-Host ""
+    Write-Host "HASH INVALIDO"
 
     exit 1
 }
 
-Write-Host "Hash válido"
+Write-Host ""
+Write-Host "HASH VALIDO"
 
+Write-Host ""
 Write-Host "Instalando GLPI Agent..."
 
 Start-Process `
@@ -42,20 +66,24 @@ Start-Process `
     -ArgumentList "/i `"$MSI`" /qn" `
     -Wait
 
-Write-Host "Verificando serviço..."
+Start-Sleep 10
 
 $Servico =
-    Get-Service `
+Get-Service `
     -Name "glpi-agent" `
     -ErrorAction SilentlyContinue
 
 if ($Servico)
 {
+    Write-Host ""
     Write-Host "GLPI Agent instalado com sucesso"
+
+    $Servico
 }
 else
 {
-    Write-Host "Falha na instalação"
+    Write-Host ""
+    Write-Host "Falha ao localizar servico"
 
     exit 1
 }
