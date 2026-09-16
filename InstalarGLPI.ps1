8e10f79533b8d238f8d982be02b6144b5bf45b78
v1BrN <bruno-santana1992@hotmail.com>
2026-09-16 18:09:43 -0300

    InstalarGLPI v0.7

--- InstalarGLPI.ps1
+++ InstalarGLPI.ps1
@@ -63,7 +63,7 @@ Write-Host "Instalando GLPI Agent..."
 
 Start-Process `
     msiexec.exe `
-    -ArgumentList "/i `"$MSI`" /qn" `
+    -ArgumentList "/i `"$MSI`" /qn SERVER=https://suporte.zelusnet.com.br:8443 RUNNOW=1" `
     -Wait
 
 Start-Sleep 10
